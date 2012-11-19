e3d367616e38b2ef861bfc0f47962d8862cef86a
Victor - work <victor@vi-design.net>
2012-11-19 18:21:56 +1100

    working on type="number" polyfill

--- slide-in/js/wpmudevui.js
+++ slide-in/js/wpmudevui.js
@@ -15,9 +15,31 @@ var wpmudevui = {
 			return s.typenum = i.type !=="text";  
 		},
 
-		init : function()
+		// input type="number" polyfill
+		typeNumPoly : function()
 		{
 			this.checkNumSupport();
+			var els = document.getElementsByTagName('input'),
+			      l = els.length,
+			   fill = document.createElement('span'); 
+
+			if (!(this.supports.typenum)) {
+				console.log('no typenum support, run polyfill');
+				for (var i=0; i<l; i++){
+					if (els[i].getAttribute('type') === "number") {
+						console.log('hit number');
+						fill.className = 'arrow-controller';
+						fill.innerHTML = '<div class="polyarrow-up"></div><div class="polyarrow-down"></div>';
+						els[i].parentNode.insertBefore(fill, els[i].nextSibling);
+					}
+				}
+			}
+		},
+
+		// init 
+		init : function()
+		{
+			this.typeNumPoly();
 		}
 
 }; // wpmudevui object definition
